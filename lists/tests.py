3bf862619e009f200584e56224b984188f8c5cc1
Chen Ming <uchenm@163.com>
2018-02-13 15:46:22 +0800

    urls

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,4 @@
-from django.core.urlresolvers import resolve
+from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
